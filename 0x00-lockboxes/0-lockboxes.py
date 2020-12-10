34e3731728a34c8cb1749b42d40ca5b4f11bc9db
matxa <42968545+matxa@users.noreply.github.com>
2020-12-09 19:46:37 -0500

    check if 1st index is a list

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -16,10 +16,15 @@ def canUnlockAll(boxes):
     """
     if not boxes:
         return False
+    if len(boxes) == 1:
+        if type(boxes[0]) != list:
+            return False
     box_numbers = [num for num in range(1, len(boxes))]
     box_keys = []
     for index_of_box in range(1, len(boxes)):
         for box in boxes:
+            if type(box) != list:
+                return False
             if boxes.index(box) != index_of_box:
                 for key in box:
                     if key == index_of_box:
