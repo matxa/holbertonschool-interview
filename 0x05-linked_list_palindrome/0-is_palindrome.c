29ee00ce2bb6e2f7f3c0a165223d81212130082f
matxa <42968545+matxa@users.noreply.github.com>
2021-01-20 01:09:33 -0500

    reverse the list and check for similarity

--- 0x05-linked_list_palindrome/0-is_palindrome.c
+++ 0x05-linked_list_palindrome/0-is_palindrome.c
@@ -1,5 +1,6 @@
 #include "lists.h"
 
+
 /**
  * is_palindrome - check if list is palindrome
  * @head: head of the list
@@ -7,75 +8,52 @@
  */
 int is_palindrome(listint_t **head)
 {
-	int length = list_length(head);
-	int *array = linked_list_to_array(head);
-	int scan_left = 0;
-	int scan_right = length - 1;
+	listint_t *reversed_list = *head;
 
-	if (head == NULL || length < 2)
-		return (1);
+	reverse_listint(&reversed_list);
 
-	if (length % 2 == 0)
+	while ((*head) != NULL && reversed_list != NULL)
 	{
-		while ((scan_left <= length / 2) && (scan_right >= length / 2))
+		if ((*head)->n != reversed_list->n)
 		{
-			if (array[scan_left] != array[scan_right])
-				return (0);
-			scan_left += 1;
-			scan_right -= 1;
+			printf("ORIG - %d <-> REV - %d\n", (*head)->n, reversed_list->n);
+			return (0);
 		}
-	}
-	else
-	{
-		while ((scan_left < length / 2) && (scan_right > length / 2))
-		{
-			if (array[scan_left] != array[scan_right])
-				return (0);
-			scan_left += 1;
-			scan_right -= 1;
-		}
+		(*head) = (*head)->next;
+		reversed_list = reversed_list->next;
 	}
 
 	return (1);
 }
 
-/**
- * list_length - length of a list
- * @head: head of the list
- * Return: length of list
- */
-int list_length(listint_t **head)
-{
-	int list_len = 0;
-	listint_t *temp = *head;
-
-	while (temp != NULL)
-	{
-		list_len += 1;
-		temp = temp->next;
-	}
-
-	return (list_len);
-}
 
 /**
- * linked_list_to_array - make linked list into array
- * @head: head of the list
- * Return: 1 if True else 0
+ * reverse_listint - reverse list
+ * @head: - head
+ * Return: list
  */
-int *linked_list_to_array(listint_t **head)
+listint_t *reverse_listint(listint_t **head)
 {
-	int array_size = list_length(head);
-	int *array = malloc(array_size * sizeof(int));
-	listint_t *temp = *head;
-	int i = 0;
+	listint_t *fir;
+	listint_t *res;
 
-	while (i != array_size && temp != NULL)
+	if (*head == NULL)
+		return (NULL);
+
+	fir = *head;
+	res = fir->next;
+
+	if (res == NULL)
 	{
-		array[i] = temp->n;
-		i += 1;
-		temp = temp->next;
+		return (NULL);
 	}
 
-	return (array);
+	reverse_listint(&res);
+	fir->next->next = fir;
+
+	fir->next = NULL;
+
+	*head = res;
+
+	return (res);
 }
--- 0x05-linked_list_palindrome/lists.h
+++ 0x05-linked_list_palindrome/lists.h
@@ -23,7 +23,6 @@ listint_t *add_nodeint_end(listint_t **head, const int n);
 void free_listint(listint_t *head);
 
 int is_palindrome(listint_t **head);
-int list_length(listint_t **head);
-int *linked_list_to_array(listint_t **head);
+listint_t *reverse_listint(listint_t **head);
 
 #endif /* LISTS_H */
