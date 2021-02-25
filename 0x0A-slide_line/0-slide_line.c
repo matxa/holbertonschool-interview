9475f0e75bb12b2c33d312b097fd10c7e48ea69d
matxa <42968545+matxa@users.noreply.github.com>
2021-02-25 16:52:37 -0500

    first commit

---
+++ 0x0A-slide_line/0-slide_line.c
@@ -0,0 +1,89 @@
+#include "slide_line.h"
+
+
+/**
+ * slide_line - slides and merges an array of integers
+ * @line: array to slide
+ * @size: size of ^ line array
+ * @direction: SLIDE_LEFT || SLIDE_RIGHT
+ * Return: 1 if success else 0
+ */
+int slide_line(int *line, size_t size, int direction)
+{
+	size_t prev;
+	size_t current;
+	size_t temp;
+	size_t incrementor;
+
+	if (size < 2)
+		return (1);
+
+	incrementor = size - 1;
+	while (incrementor != 0)
+	{
+		prev = incrementor;
+		current = incrementor - 1;
+
+		if (line[current] == line[prev])
+		{
+			temp = line[prev];
+			line[current] = line[prev] + line[current];
+			line[prev] = 0;
+			push_zeros_to_end(line, size);
+		}
+		incrementor--;
+	}
+
+	if (direction == 1)
+		return (1);
+
+	if (direction == 0)
+	{
+		push_zeros_to_start(line, size);
+		return (1);
+	}
+
+	return (0);
+}
+
+/**
+ * push_zeros_to_end - push all zero to end of array
+ * @arr: array
+ * @n: size of array
+ */
+void push_zeros_to_end(int arr[], size_t n)
+{
+	size_t count = 0;
+	size_t i;
+
+	for (i = 0; i < n; i++)
+		if (arr[i] != 0)
+			arr[count++] = arr[i];
+
+	while (count < n)
+		arr[count++] = 0;
+}
+
+/**
+ * push_zeros_to_start - push all zero to start of array
+ * @arr: array
+ * @n: size of array
+ */
+void push_zeros_to_start(int arr[], size_t n)
+{
+	int i, j;
+
+	for (j = i = n - 1 ; i >= 0 ; i--)
+	{
+		if (arr[i] == 0)
+			continue;
+		arr[j] = arr[i];
+		j--;
+	}
+
+	while (j >= 0)
+	{
+		arr[j] = 0;
+		j--;
+	}
+}
---
+++ 0x0A-slide_line/slide_line.h
@@ -0,0 +1,15 @@
+#ifndef SLIDE_LINE_H
+#define SLIDE_LINE_H
+
+/* MACROS */
+#define SLIDE_LEFT 1
+#define SLIDE_RIGHT 0
+
+#include <stdlib.h>
+#include <stdio.h>
+
+int slide_line(int *line, size_t size, int direction);
+void push_zeros_to_end(int arr[], size_t n);
+void push_zeros_to_start(int arr[], size_t n);
+
+#endif /* SLIDE_LINE_H */
