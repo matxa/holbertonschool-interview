852e8fcb9af1ad18011bb130be81967d2dbe1fe0
matxa <42968545+matxa@users.noreply.github.com>
2021-04-01 23:47:06 -0400

    first commit - file structure

---
+++ 0x0E-linear_skip/0-linear_skip.c
@@ -0,0 +1,13 @@
+#include "search.h"
+
+
+/**
+ * linear_skip - linear skip
+ * @list: head of node list
+ * @value: searchable value
+ * Return: pointer to value
+ */
+skiplist_t *linear_skip(skiplist_t *list, int value)
+{
+
+}
