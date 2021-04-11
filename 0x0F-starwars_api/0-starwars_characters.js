fcdf00d244f59e7cd1df8e93e9453a87c00d2bee
matxa <42968545+matxa@users.noreply.github.com>
2021-04-11 13:42:38 -0400

    use axios along side request

--- 0x0F-starwars_api/0-starwars_characters.js
+++ 0x0F-starwars_api/0-starwars_characters.js
@@ -1,20 +1,25 @@
 #!/usr/bin/node
 // script that prints all characters of a Star Wars movie
 const request = require('request');
+const axios = require('axios');
 
 const endPoint = 'https://swapi-api.hbtn.io/api/films/';
-const filmNumber = process.argv[2];
+const filmNumber = process.argv[2] ? process.argv[2] : null;
+const url = endPoint + filmNumber;
 
-request(`${endPoint + filmNumber}`, (error, response, body) => {
+request(url, async function (error, response, body) {
   if (!error && response.statusCode === 200) {
-    const data = JSON.parse(body);
-    for (const actor of data.characters) {
-      request(actor, (error, response, body) => {
-        if (!error && response.statusCode === 200) {
-          const person = JSON.parse(body);
-          console.log(person.name);
-        }
-      });
+    body = JSON.parse(body);
+
+    for (const character in body.characters) {
+      await actor(body.characters[character]);
     }
   }
 });
+
+async function actor (url) {
+  await axios.get(url)
+    .then(function (response) {
+      console.log(response.data.name);
+    });
+}
