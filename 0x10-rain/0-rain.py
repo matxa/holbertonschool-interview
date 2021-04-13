3e42d70378ec931d12de39ec4bd2e7660dacb56c
matxa <42968545+matxa@users.noreply.github.com>
2021-04-13 05:39:19 -0400

    first commit

---
+++ 0x10-rain/0-rain.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+""" Given a list of non-negative integers representing walls of width 1,
+calculate how much water will be retained after it rains.
+"""
+from typing import List
+
+
+def rain(walls: List) -> int:
+    """
+    Parameters:
+        walls: ist of non-negative integers
+
+    Returns:
+        int: indicating total amount of rainwater retained
+    """
+    walls_length = len(walls)
+
+    if walls_length < 1 or walls is None:
+        return 0
+
+    retained_water = 0
+    walls.sort()
+
+    for i in range(walls_length):
+        if i + 1 < walls_length:
+            if walls[i] > 0 and walls[i + 1] >= walls[i]:
+                retained_water += walls[i]
+
+    return retained_water
---
+++ 0x10-rain/0_main.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+0_main
+"""
+rain = __import__('0-rain').rain
+
+if __name__ == "__main__":
+    walls = [0, 1, 0, 2, 0, 3, 0, 4]
+    print(rain(walls))
+    print("--End--")
+    walls = [12, 4, 0, 1, 0, 2, 0, 3, 0, 4, 5, 9]
+    print(rain(walls))
