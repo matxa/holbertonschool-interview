0b87b649800131abdc254b77cc9f603fa61689c2
matxa <42968545+matxa@users.noreply.github.com>
2021-04-29 21:31:19 -0400

    use only one loop

--- 0x12-advanced_binary_search/0-advanced_binary.c
+++ 0x12-advanced_binary_search/0-advanced_binary.c
@@ -1,61 +1,61 @@
 #include "search_algos.h"
-
 /**
- * binary_search - linear search function
- * @array: array to iterate
- * @size: size of the array
- * @value: value to seach for in the array
- * Return: index
+ * print_array - print array
+ * @array: array
+ * @start: start index
+ * @end: end of array
+ * Return: Void
  */
-int binary_search(int *array, size_t size, int value)
+void print_array(int *array, size_t start, size_t end)
 {
-	size_t start = 0;
-	size_t mid;
-	size_t end = size - 1;
-	size_t i;
+	printf("Searching in array: ");
+	while (start < end)
+	{
+		printf("%i, ", array[start]);
+		start++;
+	}
+	printf("%i\n", array[start]);
+}
+/**
+ * binary_search - recursive binary search of array
+ * @array: array to search
+ * @first_num: first elements of array
+ * @second_num: second elements of array
+ * @value: value
+ * Return: index of value or [ -1 ]
+ */
+int binary_search(int *array, int first_num, int second_num, int value)
+{
+	int mid;
 
-	if (array == NULL)
+	print_array(array, first_num, second_num);
+
+	mid = (second_num - first_num) / 2 + first_num;
+
+	if (first_num == second_num)
 		return (-1);
 
-	while (start <= end)
-	{
-		printf("Searching in array: ");
+	if (array[mid] == value && array[mid - 1] != value)
+		return (mid);
 
-		for (i = start; i < end + 1; i++)
-		{
-			if (i < end)
-			{
-				printf("%d", array[i]);
-				printf(", ");
-			}
-			else
-			{
-				printf("%d", array[i]);
-				printf("\n");
-			}
-		}
+	if (array[mid] >= value)
+		return (binary_search(array, first_num, mid, value));
 
-		mid = (start + end) / 2;
-		if (array[mid] == value)
-			return (mid);
-		if (value < array[mid])
-			end = mid - 1;
-		if (value > array[mid])
-			start = mid + 1;
-	}
+	if (array[mid] <= value)
+		return (binary_search(array, mid + 1, second_num, value));
+
 	return (-1);
 }
-
 /**
  * advanced_binary - advanced search
  * @array: array to search
- * @size: size of array
- * @value: value to return
- * Return: index of 1st value in array or -1 on fail
+ * @size: size
+ * @value: value
+ * Return: index of value or [ -1 ]
  */
 int advanced_binary(int *array, size_t size, int value)
 {
 	if (array == NULL)
 		return (-1);
-	return (binary_search(array, size, value));
+	return (binary_search(array, 0, size - 1, value));
 }
