dc79de22dbfb40d7054d35c6d7fa4b701ab5f495
matxa <42968545+matxa@users.noreply.github.com>
2021-06-03 22:37:55 -0400

    first commit

---
+++ 0x17-find_the_loop/0-find_loop.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * find_listint_loop - find the loop in the list
+ * @head: head of linked list
+ * Return: pointer listint_t
+ */
+listint_t *find_listint_loop(listint_t *head)
+{
+	listint_t *p1 = head;
+	listint_t *p2 = head;
+
+	while (p2 && p2->next)
+	{
+		p1 = p1->next;
+		p2 = p2->next->next;
+		if (p1 == p2)
+			return (p1);
+	}
+	return (NULL);
+}
